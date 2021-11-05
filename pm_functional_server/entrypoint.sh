895448401975c12e2062d96b44f4a5216aa165f3
Влас Катенев <vlaskatenev@MacBook-Air-Vlas.local>
2021-11-05 22:59:46 +0300

    refactoring Dockerfile

--- pm_functional_server/entrypoint.sh
+++ pm_functional_server/entrypoint.sh
@@ -3,6 +3,6 @@
 # python manage.py flush --no-input
 python3 manage.py makemigrations
 python3 manage.py migrate
-python3 manage.py collectstatic --no-input --clear
+# python3 manage.py collectstatic --no-input --clear
 
 exec "$@"
